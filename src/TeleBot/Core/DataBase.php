33ef366c1d1f9aa71e793af8edf30fb6b25c00f0
projkin <54156948+projkin@users.noreply.github.com>
2022-11-15 18:51:45 +0000

    test

---
+++ src/TeleBot/Core/DataBase.php
@@ -0,0 +1,12 @@
+<?php namespace TeleBot\Core;
+
+class DB 
+{
+    
+    public function getDB()
+    {
+        return 'Get DB';
+    }
+
+
+}
---
+++ src/TeleBot/Core/Telegram.php
@@ -0,0 +1,29 @@
+<?php namespace TeleBot\Core;
+
+use DataBase;
+
+class Core
+{
+    
+    public $db;         
+
+    public function __construct(DataBase $db)
+    {
+        $this->db = $db;
+    }
+
+
+    public function getCore()
+    {
+        return 'GET Core';
+    }
+
+
+    public function getCoreDB()
+    {
+        return $this->db;
+    }
+
+
+
+}
